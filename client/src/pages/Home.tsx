2e357290c30c807329643fecf5f632678d56d177
invisiblymuted <invisiblymuted@gmail.com>
2026-01-05 05:21:14 -0700

    Fix: remove extra closing div in Home The Need section (JSX parse error)

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -174,7 +174,6 @@ export default function Home() {
                       </div>
                     </div>
                   </div>
-                  </div>
                 </div>
               </div>
             </div>
