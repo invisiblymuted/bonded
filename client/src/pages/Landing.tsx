30da71cfe45f54d752dfc4a27ae3cd5cbde81daf
invisiblymuted <invisiblymuted@gmail.com>
2026-01-04 19:18:09 -0700

    Landing: nudge banner slightly left on larger screens

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -25,7 +25,7 @@ export default function LandingPage() {
         {/* Logo Banner - centered both vertically and horizontally; nav icons stacked underneath */}
         <div className="w-full flex items-center justify-center px-4 sm:px-6 py-12">
           <div className="container mx-auto max-w-6xl">
-            <div className="flex flex-col items-center gap-6 text-center">
+            <div className="flex flex-col items-center gap-6 text-center transform md:-translate-x-4 lg:-translate-x-6">
             <Logo />
             <div className="flex items-center gap-4 mt-2 justify-center">
               <Link href="/home">
